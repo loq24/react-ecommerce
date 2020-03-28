40f57214fe605421f4b8cbe9e981dacbcc3e2a8f
LougieQ <lougie.quisel@gmail.com>
2020-03-28 17:39:45 +0800

    Fix API integration

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,6 @@ export const wooApi = new WooCommerceRestApi({
   url: 'https://react-ecommerce.lougiequisel.com',
   consumerKey: 'ck_64aa28b5e566c317b5a9a809fa6f07b19e646abc',
   consumerSecret: 'cs_a56e63dc12ab253643dc05a7c6b934024aee55cf',
-  version: 'wc/v3'
+  version: 'wc/v3',
+  queryStringAuth: true
 });
-
-export const CART_COOKIE_NAME = 'CART_ITEMS';
